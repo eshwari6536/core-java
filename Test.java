1fed1c46fd40045297633984f3f3ed9540f86f60
eshwari6536 <97502934+eshwari6536@users.noreply.github.com>
2022-04-29 14:47:19 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,18 @@
+package cooli.pak;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		Boolean b1=true;
+		Boolean b2=false;
+		if((x==4)&&!b2) 
+			System.out.println("1");
+		System.out.println("2");
+		if((b2=true)&&b1)
+		System.out.println("3");
+		}
+	}
+
+
