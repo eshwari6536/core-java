86551f15908fd0fcc2d44803c184c9146f1ec51c
eshwari6536 <97502934+eshwari6536@users.noreply.github.com>
2022-04-29 14:26:25 +0530

    Add files via upload

---
+++ HelloWrld.java
@@ -0,0 +1,14 @@
+package hw;
+
+public class HelloWrld {
+public static void main(String args[]) {
+int a = 2;
+float b = 4.5f;
+double c = 5.67;
+char d = 'A';
+System.out.println(a);//display an integer
+System.out.println(b);//display a decimal
+System.out.println(c);//display a decimal upto 2 places
+System.out.println(d);//display a character
+}
+}
